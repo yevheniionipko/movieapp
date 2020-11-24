792cca92129238349cffb322b18ddbec675cce1b
Yevhenii Onipko <y.onipko@kyivstar.net>
2020-11-24 08:55:21 +0200

    added select film by genre

--- Services/Requests.swift
+++ Services/Requests.swift
@@ -33,4 +33,30 @@ struct Requests {
             }
         }).resume()
     }
+    
+    static func getMoviesByGenre(genre: String, completionHandler: @escaping (Movie?) -> Void) -> Void {
+        
+        let request = URLRequest(url: BaseService.discoverMovieByGenre(genre: genre).path)
+        URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+            if let error = error {
+              print("Error with fetching films: \(error)")
+              return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+              
+              print("Error with the response, unexpected status code: \(String(describing: response))")
+              return
+            }
+            
+            do {
+                let res = try JSONDecoder().decode(Movie.self, from: data!)
+                completionHandler(res)
+
+            } catch let error as NSError {
+                print(error.debugDescription)
+            }
+        }).resume()
+    }
 }
